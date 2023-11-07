1e2a7a4ebbee4cebc8ae91682db9ad37d20f4a68
Ahmad <ahmad.abdalqader2000@hotmail.com>
2023-11-06 19:14:20 -0500

    init

---
+++ rnnmodel.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+
+
+
+class RNNMODEL(nn.Module):
+    def __init__(self, input_size = None, hidden_size=None, num_layers=None, readout_size=2, dropout = 0.1, gru_kwargs={}, readout_kwargs={}):
+        super().__init__()
+        params = [input_size, hidden_size, num_layers]
+        if None in params:
+            raise Exception
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.readout_size = readout_size
+        self.gru_kwargs = gru_kwargs
+        self.readout_kwargs = readout_kwargs
+        self.gru = nn.GRU(hidden_size=self.hidden_size,
+                          num_layers=self.num_layers,
+                          **gru_kwargs
+        )
+        self.readout = nn.Linear(in_features=self.hidden_size, out_features=self.readout_size)
+
+
+    def forward(self, x):
+
+        gru_out, _ = self.gru(x)
+        readout_out = self.readout(gru_out)
+        return readout_out
+
+
+    def load_model(self, filepath):
+        self.load_state_dict(torch.load(filepath))
+        self.eval()  # Set the model to evaluation mode if needed
+
+
---
+++ train_rnn.py
@@ -0,0 +1,241 @@
+#%% ----------------------------------------------------------------------------
+# Import Libraries
+# ------------------------------------------------------------------------------
+import os
+import wandb
+import torch
+import numpy as np
+import torch.nn as nn
+from pathlib import Path
+import torch.optim as optim
+import scipy.signal as signal
+from sklearn.metrics import r2_score
+from sklearn.linear_model import Ridge
+from nlb_tools.evaluation import evaluate
+from sklearn.model_selection import KFold
+from nlb_tools.nwb_interface import NWBDataset
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+from nlb_tools.make_tensors import make_train_input_tensors, make_eval_input_tensors, make_eval_target_tensors, save_to_h5
+from rnn_model import RNNMODEL
+
+
+#%% ----------------------------------------------------------------------------
+# Static Parameters
+# ------------------------------------------------------------------------------
+BINSIZE = 5 # ms - bin size for resampling
+LAG = 100 # ms - how much to delay the kinematics by
+SMOOTH_SD = 40 # ms - std of gaussian kernel for smoothing
+DATASET = 'mc_maze_small' # NLB name of dataset
+
+WANDB_PROJECT = '7610 Final Project Testing' # name of wandb project
+WANDB_ENTITY = 'forrealahmad' # wandb username (change to yours)
+
+
+#%% ----------------------------------------------------------------------------
+# Hyperparameters
+# ------------------------------------------------------------------------------
+
+RNN_LAYERS = [1, 2, 3, 4, 5] # number of layers to try for RNN
+RNN_HIDDEN_SIZE = [32, 64, 128, 256] # hidden size for RNN
+RNN_DROPOUT = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7] # dropout for RNN
+RNN_LR = [0.0001, 0.001, 0.01] # learning rate for RNN
+RNN_EPOCHS = [100, 500, 1000] # number of epochs to train RNN for
+RNN_BATCH_SIZE = [32, 64, 128, 256] # batch size for RNN
+
+
+#%% ----------------------------------------------------------------------------
+# Dataset Set Up
+# ------------------------------------------------------------------------------
+DATASET_IDS = {
+    'mc_maze': 128,
+    'mc_rtt': 129,
+    'mc_maze_large': 138,
+    'mc_maze_medium': 139,
+    'mc_maze_small': 140,
+}
+id = DATASET_IDS[DATASET]
+# get the path to the NLB data using the dataset ID
+data_folder = Path.cwd() / 'data' / f'000{id}'
+
+# download dataset if it doesn't exist
+if not data_folder.exists():
+    print(f'Dataset "{DATASET}" not downloaded! Downloading now.')
+    os.system(f'mkdir {data_folder}')
+    os.system(f'dandi download https://dandiarchive.org/dandiset/000{id} -o data')
+
+# get the path to the folder inside the data folder
+data_path = next((p for p in data_folder.iterdir() if p.is_dir()), None)
+
+#%% ----------------------------------------------------------------------------
+# Prepare / Preprocess Data
+# ------------------------------------------------------------------------------
+# Warning! This step takes ~1.5 mins
+dataset = NWBDataset(data_path) # load dataset from file
+dataset.resample(BINSIZE) # resample to 5 ms bins
+
+# extract training and validation spikes
+trainval_dict = make_train_input_tensors(dataset, DATASET, save_file=False) 
+behav_dict = make_eval_target_tensors(dataset, DATASET, save_file=False)
+
+# combine heldin and heldout neurons
+stacked = [trainval_dict[f'train_spikes_{i}'] for i in ['heldin', 'heldout']]
+trainval_spikes = np.concatenate(stacked, axis=2)
+
+# smooth spikes using gaussian window
+kern_sd = int(round(SMOOTH_SD / dataset.bin_width))
+window = signal.gaussian(kern_sd * 6, kern_sd, sym=True)
+filt = lambda x: np.convolve(x, window / np.sum(window), 'same')
+trainval_smth_spikes = np.apply_along_axis(filt, 1, trainval_spikes)
+
+# extract training and validation kinematics
+trainval_behavior = behav_dict[DATASET]['train_behavior']
+
+# delay kinematics by LAG ms
+lag_bins = int(LAG / BINSIZE) # number of bins to delay kinematics by
+trainval_smth_spikes = trainval_smth_spikes[:, :-lag_bins]
+trainval_behavior = trainval_behavior[:, lag_bins:]
+
+# split into training and validation sets
+tv_splits = train_test_split(trainval_smth_spikes, 
+                             trainval_behavior, 
+                             test_size=0.2, 
+                             random_state=1)
+train_spikes, val_spikes, train_behavior, val_behavior = tv_splits
+
+
+#%% ----------------------------------------------------------------------------
+# Prepare data for training 
+# ------------------------------------------------------------------------------
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_spikes_tensor = torch.Tensor(train_spikes, device=device)
+val_spikes_tensor = torch.Tensor(val_spikes, device=device)
+
+train_behavior_tensor = torch.Tensor(train_behavior, device=device)
+val_behavior_tensor = torch.Tensor(val_behavior, device=device)
+
+input_size = train_spikes_tensor.size()[-1] # Specify the input size based on your data
+output_size = train_behavior_tensor.size()[-1]
+
+train_dataset = TensorDataset(train_spikes, train_behavior)
+val_dataset = TensorDataset(val_spikes, val_behavior)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)  # Default batch size for initialization
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)  # Default batch size for initialization
+
+#%% -----------------------------------
+# Run a single run
+# -------------------------------------
+wandb.init(project=WANDB_PROJECT, entity=WANDB_ENTITY, name=f'RNN_0')
+
+
+# Set up the RNN model, optimizer, and loss function
+model = RNNMODEL(input_size=input_size, rnn_layers=RNN_LAYERS[0], rnn_hidden_size=RNN_HIDDEN_SIZE[0], rnn_dropout=RNN_DROPOUT[0]).to(device)
+optimizer = optim.Adam(model.parameters(), lr=RNN_LR[0])
+mse_loss = nn.MSELoss()
+
+# Training loop
+for epoch in range(RNN_EPOCHS[0]):
+    model.train()
+    total_loss = 0
+    for batch_idx, (spikes, behavior) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(spikes)
+        loss = mse_loss(output, behavior)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    # Validation loop
+    model.eval()
+    val_loss = 0
+    mse_loss = nn.MSELoss()
+    with torch.no_grad():
+        for batch_idx, (spikes, behavior) in enumerate(val_loader):
+            spikes, behavior = spikes.to(device), behavior.to(device)
+            output = model(spikes)
+            loss = mse_loss(output, behavior)
+            val_loss += loss.item()
+
+    # Log training and validation loss to Weights and Biases
+    wandb.log({"train_loss": total_loss / len(train_loader), "val_loss": val_loss / len(val_loader), "epoch": epoch})
+
+# Save the trained model
+torch.save(model.state_dict(), "rnn_model.pth")
+wandb.save("rnn_model.pth")  # Save the model to Weights and Biases
+
+wandb.finish()
+
+
+
+# #%% -----------------------------------
+# # Run a sweep on hyperparameters
+# # -------------------------------------
+
+# # Initialize Weights and Biases
+# wandb.init()
+
+# # Set up the sweep configuration
+# sweep_config = {
+#     "name": "rnn-sweep",
+#     "method": "grid",
+#     "parameters": {
+#         "rnn_layers": {"values": RNN_LAYERS},
+#         "rnn_hidden_size": {"values": RNN_HIDDEN_SIZE},
+#         "rnn_dropout": {"values": RNN_DROPOUT},
+#         "rnn_lr": {"values": RNN_LR},
+#         "rnn_epochs": {"values": RNN_EPOCHS},
+#         "rnn_batch_size": {"values": RNN_BATCH_SIZE}
+#     }
+# }
+
+# # Initialize the sweep
+# sweep_id = wandb.sweep(sweep=sweep_config, project="rnn-sweep-project")
+
+# def train():
+#     # Iterate over sweep configurations
+#     for config in wandb.controller():
+#         # Set up the RNN model with the current configuration
+#         model = RNNMODEL(
+#             input_size=input_size, 
+#             hidden_size=config['rnn_hidden_size'],
+#             num_layers=config.parameters['rnn_layers'],
+#             dropout=config['rnn_dropout'])
+
+
+#         criterion = nn.CrossEntropyLoss()
+#         optimizer = optim.Adam(model.parameters(), lr=config['rnn_lr'])
+
+#         # Training loop
+#         for epoch in range(config['rnn_epochs']):
+#             model.train()
+#             total_loss = 0
+#             for batch_idx, (spikes, behavior) in enumerate(train_loader):
+#                 optimizer.zero_grad()
+#                 output = model(spikes)
+#                 loss = criterion(output, behavior)
+#                 loss.backward()
+#                 optimizer.step()
+#                 total_loss += loss.item()
+
+#             # Log the training loss to Weights and Biases
+#             wandb.log({"train_loss": total_loss / len(train_loader), "epoch": epoch})
+
+#             # Validation loop
+#             model.eval()
+#             val_loss = 0
+#             with torch.no_grad():
+#                 for batch_idx, (spikes, behavior) in enumerate(val_loader):
+#                     output = model(spikes)
+#                     loss = criterion(output, behavior)
+#                     val_loss += loss.item()
+
+#             # Log the validation loss to Weights and Biases
+#             wandb.log({"val_loss": val_loss / len(val_loader), "epoch": epoch})
+
+#         # Log the sweep configuration and final metrics to Weights and Biases
+#         wandb.log(config)
+
+# # Run the sweep
+# wandb.agent(sweep_id, function=train)
